6ea4d0762b76b348891955e740c2a55fabaaa93d
psiphonc <psiphonc@outlook.com>
2019-06-15 20:49:17 +0800

    promote added

--- src/CircularQueue.java
+++ src/CircularQueue.java
@@ -10,17 +10,31 @@ public class CircularQueue<T> {
     private int out = 0;
 
     void offer(T e) throws QueueFullException {
-        if (cnt > capacity) throw new QueueFullException();
+        if (cnt > capacity) {
+            show();
+            throw new QueueFullException();
+        }
         arr[in] = e;
         cnt++;
         in = (in + 1) % capacity;
     }
 
     T poll() throws QueueEmptyException{
-        if (cnt == 0) throw new QueueEmptyException();
+        if (cnt == 0) {
+            show();
+            throw new QueueEmptyException();
+        }
         T ret = arr[out];
         out = (out + 1) % capacity;
         cnt--;
         return ret;
+    }
+    void show(){
+        System.out.printf(String.format("length:%d;", arr.length));
+        for (T i :
+                arr) {
+            System.out.printf("%d ",i);
+        }
+        System.out.println();
     }
 }
